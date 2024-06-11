d7857b2bb83c18b5f1e46c720c700d3e665673c5
김재우 <rlawodn6548@naver.com>
2024-06-12 00:09:22 +0900

    Mocktio Verify 테스트
    - 메소드 최대 최소 호출 횟수 검증

--- src/test/java/com/example/JavaTestApplication/verifying/count/CountNumberOfInvocationTest.java
+++ src/test/java/com/example/JavaTestApplication/verifying/count/CountNumberOfInvocationTest.java
@@ -36,5 +36,25 @@ public class CountNumberOfInvocationTest {
 
         // mockedList.add("three times")를 세 번 호출했는지 검사
         Mockito.verify(mockedList, Mockito.times(3)).add("three times");
+
+        // mockedList.add("never")를 호출안했는지 검사
+        Mockito.verify(mockedList, Mockito.never()).add("never");
+
+        // 적어도 몇 번 또는 최대 몇 번 호출했는지 검사
+        Mockito.verify(mockedList, Mockito.atMostOnce()).add("once");
+        Mockito.verify(mockedList, Mockito.atLeastOnce()).add("twice");
+        Mockito.verify(mockedList, Mockito.atMost(5)).add("three times");
+        Mockito.verify(mockedList, Mockito.atLeast(2)).add("three times");
+
+    }
+
+    @Test
+    void neverCallVerifyTest() {
+        // given
+        ArrayList mockedList = Mockito.mock(ArrayList.class);
+
+        // when
+
+        // then
     }
 }
